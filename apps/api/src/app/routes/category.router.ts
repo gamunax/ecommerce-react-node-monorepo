a0ee644b4d2669370c7a91f1f19986a851668420
Jan Pierre <gamunaxx@gmail.com>
2022-01-04 22:15:50 -0500

    feat(api): implement category

--- apps/api/src/app/routes/category.router.ts
+++ apps/api/src/app/routes/category.router.ts
@@ -1,11 +1,17 @@
 import * as express from 'express';
 const router = express.Router();
 
-const { create } = require('../controllers/category.controller');
+const { create, categoryById, read, update, remove, list } = require('../controllers/category.controller');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth.controller');
 const { userById } = require('../controllers/user.controller');
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.get('/categories', list);
+
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;
